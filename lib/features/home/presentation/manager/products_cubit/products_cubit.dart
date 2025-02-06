bb9f8aba99f7bd3f1b4d214ebdf021fad134799c
sherifshouaib <sherifshouaib22@gmail.com>
2025-02-06 15:35:56 +0200

    create products method

--- lib/features/home/presentation/manager/products_cubit/products_cubit.dart
+++ lib/features/home/presentation/manager/products_cubit/products_cubit.dart
@@ -1,9 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:store_app/features/home/data/models/product_model/product_model.dart';
+import 'package:store_app/features/home/data/repos/home_repo.dart';
 
 part 'products_state.dart';
 
 class ProductsCubit extends Cubit<ProductsState> {
-  ProductsCubit() : super(ProductsInitial());
+  ProductsCubit(this.homeRepo) : super(ProductsInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> getAllProducts() async {
+    emit(ProductsLoading());
+
+    var result = await homeRepo.getAllProducts();
+
+    result.fold((failure) {
+      emit(ProductsFailure(failure.errMessage));
+    }, (products) {
+      emit(ProductsSuccess(products));
+    });
+  }
 }
