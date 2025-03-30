8e5099ebbb8d61cc21d2074543de8eb0829149aa
sherifshouaib <sherifshouaib22@gmail.com>
2025-03-30 15:45:11 +0200

    task solution

--- lib/features/auth/presentation/views/widgets/profile_picture_design.dart
+++ lib/features/auth/presentation/views/widgets/profile_picture_design.dart
@@ -15,9 +15,8 @@ class ProfilePictureDesign extends StatefulWidget {
 class _ProfilePictureDesignState extends State<ProfilePictureDesign> {
   File? imgPath;
 
-  uploadImage2Screen() async {
-    final pickedImg =
-        await ImagePicker().pickImage(source: ImageSource.gallery);
+  uploadImage2Screen(ImageSource sourcee) async {
+    final pickedImg = await ImagePicker().pickImage(source: sourcee);
     try {
       if (pickedImg != null) {
         setState(() {
@@ -29,6 +28,66 @@ class _ProfilePictureDesignState extends State<ProfilePictureDesign> {
     } catch (e) {
       print("Error => $e");
     }
+
+   // Navigator.pop(context);
+  }
+
+  showModel() {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          padding: const EdgeInsets.all(22),
+          height: 170,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: () async {
+                  await uploadImage2Screen(ImageSource.camera);
+                },
+                child: const Row(
+                  children: [
+                    Icon(
+                      Icons.camera,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 11,
+                    ),
+                    Text('From Camera'),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 22,
+              ),
+              GestureDetector(
+                onTap: () async {
+                  await uploadImage2Screen(ImageSource.gallery);
+                },
+                child: const Row(
+                  children: [
+                    Icon(
+                      Icons.photo_outlined,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 11,
+                    ),
+                    Text('From Gallery'),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 22,
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 
   @override
@@ -49,7 +108,8 @@ class _ProfilePictureDesignState extends State<ProfilePictureDesign> {
             bottom: -10,
             child: IconButton(
               onPressed: () {
-                uploadImage2Screen();
+                //   uploadImage2Screen();
+                showModel();
               },
               icon: const Icon(Icons.add_a_photo),
               color: const Color.fromARGB(255, 94, 115, 128),
